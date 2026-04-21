0e0c3250c8e77e28b2e2b0800f8d2defdccef0d1
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-04-21 17:55:23 +0800

    refactor(Hero): update heading structure and improve text presentation

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -5,14 +5,15 @@ export const Hero = () => {
   return (
     <section className="mx-auto max-w-5xl px-4 py-24">
       <Reveal>
-        <h1 className="text-5xl font-bold tracking-tight text-primary">
-          Hi! I'm <span className="text-primary">Nethan 🙋‍♂️</span>
+        <h1 className="text-5xl font-bold tracking-tight text-primary leading-none">
+          Hi! I'm <span className="text-primary">Nethan</span>
+          <span className="relative -top-1">👋🏼</span>
         </h1>
       </Reveal>
       <Reveal delay={0.1}>
         <p className="mt-4 max-w-xl text-lg text-muted-foreground">
           I'm a Full-Stack Dev and a graduating (June 2026) Computer Science
-          student. Appreciate you for visiting my personal website!
+          student. Appreciate you for visiting my personal website!{" "}
         </p>
       </Reveal>
       <Reveal delay={0.2}>
