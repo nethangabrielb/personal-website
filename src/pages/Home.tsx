eb6c269d546b17f653c747dca3b194b63af8c671
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-04-24 23:03:41 +0800

    feat(HomePage): add Projects component to HomePage layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/layout/Hero";
+import { Projects } from "@/components/layout/Projects";
 import { TechStack } from "@/components/layout/TechStack";
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
-      <TechStack></TechStack>
+      <TechStack />
+      <Projects />
     </>
   );
 };
